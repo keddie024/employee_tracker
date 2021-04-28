b4a994a0bc30ae0bf2f38003facab55a7b00eac0
Kevin Eddie <keddie024@gmail.com>
2021-04-27 21:21:15 -0400

    add role function complete

--- js/add.js
+++ js/add.js
@@ -20,9 +20,52 @@ function addDepartment(connection, init) {
 
 };
 
-function addRole() {
+function addRole(connection, init) {
+    let newRole = {};
+    connection.query("SELECT * FROM department", function (err, res) {
+        inquirer
+            .prompt([
+                {
+                    name: "role",
+                    type: "input",
+                    message: "What role would you like to add?"
+                },
+                {
+                    name: "salary",
+                    type: "input",
+                    message: "What will be the salary of the role?",
+                },
+                {
+                    name: "department",
+                    type: "list",
+                    choices: function () {
+                        let choiceArray = [];
+                        for (var i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "Which department will the role be added to?"
+                }
+            ])
+            .then(function (response) {
+                newRole.title = response.role;
+                newRole.salary = response.salary;
 
-}
+                connection.query("SELECT id FROM department WHERE name = ?", response.department, function (err, res) {
+                    if (err) throw err;
+                    newRole.department_id = res[0].id;
+
+                    connection.query('INSERT INTO role SET ?', newRole, function (err, res) {
+                        if (err) throw err;
+                        console.log("Role added!");
+                        init();
+                    });
+                })
+
+            })
+    })
+};
 
 function addEmployee() {
 
