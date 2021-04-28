ac588c42dc8a4429e7e60265481a86cdbccf9cbd
Kevin Eddie <keddie024@gmail.com>
2021-04-27 22:29:16 -0400

    view employee function complete

--- js/view.js
+++ js/view.js
@@ -18,9 +18,14 @@ function viewRole(connection, init) {
     });
 };
 
-function viewEmployee() {
-
-}
+function viewEmployee(connection, init) {
+    let query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, e2.first_name AS manager FROM employee LEFT JOIN employee as e2 ON e2.id = employee.manager_id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        init();
+    });
+};
 
 module.exports = {
     viewDepartment: viewDepartment,
